c1222cd7ccb0620ba8ba453ce2839ba2848ed679
addetz <adelinasimion@gmail.com>
2022-10-16 09:27:32 +0300

    add chapter 3 solution

---
+++ chapter03/format/error_test.go
@@ -0,0 +1,22 @@
+package format_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/PacktPublishing/Test-Driven-Development-in-Go/chapter03/format"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestError(t *testing.T) {
+	// Arrange
+	initialErr := errors.New("error msg")
+	expr := "2%3"
+
+	// Act
+	wrappedErr := format.Error(expr, initialErr)
+
+	// Assert
+	assert.Contains(t, wrappedErr.Error(), initialErr.Error())
+	assert.Contains(t, wrappedErr.Error(), expr)
+}
